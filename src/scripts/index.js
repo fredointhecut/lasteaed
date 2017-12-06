cc896d12283c5ad1481c92b0bc87454c7799a338
fredo <fredi.vainumae1@gmail.com>
2017-12-06 18:55:41 +0200

    algus

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,6 @@
-console.log('Hello');
+const string = 'Hello dog';
+const number = 21.222313;
+const func = (a, b) => `${a} ${b * 2}`;
+const stringPlusNumber = func(string, number);
+
+console.debug(stringPlusNumber);
